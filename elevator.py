3d9aa37c8cb5e97387b5bdb7bc29d57e2dd6bc95
cattia <you@example.com>
2020-03-31 17:05:32 -0400

    [cricri] elevator

---
+++ elevator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+class Elevator:
+    #NbPlaces : Nombres de places dans l'ascenseur
+    #Vitesse : Vitesse de l'ascenseur pour pour aller d'un étage à l'autre.
+    #etageActuel : Pour savoir ou est l'ascenseur
+    #coursUtilisation : Boolean 
+    def __init__(self,nbPlaces,vitesse,etageActuel,coursUtilisation,etageChoisi,nbEtage):
+        super().__init__()
+        
+    def FirstComeFirstServe(self):
+        return 0
+    def shortestSeekTimeFirst(self):
+        return 0
+    def LinearScan(self):
+        return 0
